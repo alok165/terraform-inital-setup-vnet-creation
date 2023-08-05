dfbc9cadc3f60b59d1ce9a46ff94334687b2ab5a
alokaz400 <alok165@gmail.com>
2023-08-05 18:08:42 +1000

    removed the client id, client secret, tenant id and sub id from the code as it set into workspace

--- main.tf
+++ main.tf
@@ -14,11 +14,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
-  subscription_id = "19918177-652b-4212-bd55-d104b45290e5"
-  tenant_id       = "c0da05fc-d443-4445-9f3c-bcd2b45aeb1a"
-  client_secret   = "6XM8Q~yPpicA8oeDWoBRHvscR3tOZVgd0WJugbnF"
-  client_id       = "91e08505-aa26-43a3-9170-e435b8a1f0ff"
 }
 # Create a resource group
 resource "azurerm_resource_group" "rgtest1" {
